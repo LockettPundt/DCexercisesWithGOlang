2cf24cc859be006dbd43bbeb158b91f5117d7cf6
Lockett Pundt <kalifornium@hotmail.com>
2020-06-30 18:58:37 -0400

    main entry

--- main/main.go
+++ main/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println(banner("Hi Lockett!"))
+	// fmt.Println(banner("Hi Lockett!"))
+	// fmt.Println(l337speak("Hi Lockett! How Are You?"))
+	// fmt.Println(listmultiplier(12))
+	// fmt.Println(printbox(20, 10))
+	// fmt.Println(printsquare(13, 13))
+	// fmt.Println(triangleprinter(30))
+	// arr := []int{1, -2, 3, 16, -100, 600}
+	// fmt.Println(smallestnuminarray(arr))
 }
